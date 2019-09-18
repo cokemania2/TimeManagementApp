ade7667448f3ae8144c5c75e3ff667230730c4dd
cokemania <coke_mania@naver.com>
2019-09-18 15:28:52 +0900

    update

--- app/src/main/java/com/example/myapplication/userMain.java
+++ app/src/main/java/com/example/myapplication/userMain.java
@@ -18,11 +18,37 @@ public class userMain extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user_main);
         ActionBar ab = getSupportActionBar() ;
+
+        myDialog = new Dialog(this);
+
         ab.setTitle("test") ;
         ab.setIcon(R.drawable.gucc3) ;
         ab.setDisplayUseLogoEnabled(true) ;
         ab.setDisplayShowHomeEnabled(true) ;
 
+        Button buttonn = findViewById(R.id.popup);
+        buttonn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ShowPopup(view);
+            }
+        });
+
+    }
+
+    public void ShowPopup(View v) {
+        TextView txtclose;
+        myDialog.setContentView(R.layout.custompopup);
+        txtclose =(TextView) myDialog.findViewById(R.id.txtclose);
+        txtclose.setText("X");
+        txtclose.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                myDialog.dismiss();
+            }
+        });
+        myDialog.getWindow().setBackgroundDrawable(new ColorDrawable(Color.TRANSPARENT));
+        myDialog.show();
     }
 
 }
